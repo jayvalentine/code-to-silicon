9419a8c3a8a38a2765d5dbc701939af9fbbdfe30
jayvalentine <jayv136@gmail.com>
2019-01-20 14:02:34 +0000

    got code compiling and dumping to ASM now.

--- code-to-silicon.py
+++ code-to-silicon.py
@@ -1,3 +1,7 @@
 import compile
 
-compile.gcc_help()
+compile.gcc_compile(["c/main.c"], "main.o")
+
+compile.gcc_link(["main.o"], "main.elf")
+
+compile.objdump("main.elf", ["-d"], "main.asm")
--- compile.py
+++ compile.py
@@ -5,6 +5,8 @@ XILINX_GNU_PREFIX = "microblazeel-xilinx-linux-gnu-"
 
 GCC = XILINX_GNU_PREFIX + "gcc"
 
+OBJDUMP = XILINX_GNU_PREFIX + "objdump"
+
 def run_command(path, flags):
   cmd = path + " " + " ".join(flags)
   print(cmd)
@@ -12,3 +14,12 @@ def run_command(path, flags):
 
 def gcc_help():
   run_command(GCC, ["--help"])
+
+def gcc_compile(files, output):
+  run_command(GCC, ["-c"] + files + ["-o " + output])
+
+def gcc_link(files, output):
+  run_command(GCC, files + ["-o " + output])
+
+def objdump(file, flags, output):
+  run_command(OBJDUMP, [file] + flags + ["> " + output])
