8fe0feacd22d3def79d709e47e85aa17ed44ae75
jayvalentine <jayv136@gmail.com>
2019-01-29 15:27:59 +0000

    renamed gcc_compile() to compile()

--- toolchain/compiler.py
+++ toolchain/compiler.py
@@ -5,5 +5,5 @@ GCC = common.XILINX_GNU_PREFIX + "gcc"
 def help():
   common.run_command(GCC, ["--help"])
 
-def gcc_compile(files, output):
+def compile(files, output):
   common.run_command(GCC, ["-S"] + files + ["-o " + output])
