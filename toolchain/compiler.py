1dccd06dbaef4f48ee247d14bc30a9a2dca654ed
jayvalentine <jayv136@gmail.com>
2019-02-07 13:02:40 +0000

    Playing with compiler options

--- toolchain/compiler.py
+++ toolchain/compiler.py
@@ -6,7 +6,7 @@ def help():
   common.run_command(GCC, ["--help"])
 
 def compile(files, output):
-  common.run_command(GCC, ["-S -Xassembler -ahlsm -mno-xl-soft-div -mno-xl-soft-mul -nostartfiles -fno-delayed-branch -flive-range-shrinkage -funroll-all-loops -Os"] + files + ["-o " + output])
+  common.run_command(GCC, ["-S -Xassembler -ahlsm -mno-xl-soft-div -mhard-float -mno-xl-soft-mul -nostartfiles -fno-delayed-branch -flive-range-shrinkage -funroll-all-loops -Os"] + files + ["-o " + output])
 
 def assemble(files, output):
   common.run_command(GCC, files + ["-o " + output])
