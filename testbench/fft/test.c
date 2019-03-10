07d9acdd891c4f66273b3fbf5df943f15f7a73f9
jayvalentine <jayv136@gmail.com>
2019-03-10 12:04:52 +0000

    Fixed missing header in FFT test.

--- testbench/fft/test.c
+++ testbench/fft/test.c
@@ -1,7 +1,3 @@
-#include <stdint.h>
-
-#include "application_vars.h"
-
 // This is the only way I can think of to force the compiler
 // not to combine test_failed and test_passed.
 volatile int dummy;
